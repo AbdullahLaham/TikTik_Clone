cd6de694260f6be42003548b2e0238cc5edd8b37
AbdullahLaham <82181597+AbdullahLaham@users.noreply.github.com>
2022-08-23 10:06:02 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  trailingSlash: true,
   swcMinify: true,
   images: {
     domains: ['yth3.ggpht.com', 'lh3.googleusercontent.com'],
