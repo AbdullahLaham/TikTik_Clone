d37e867b6dbf022056f1e33ed4a944bd3cc20882
AbdullahLaham <82181597+AbdullahLaham@users.noreply.github.com>
2022-08-23 15:09:30 +0300

    Update index.js

--- pages/api/post/index.js
+++ pages/api/post/index.js
@@ -1,7 +1,7 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 import { allPostsQuery } from '../../../utils/queries';
-import { client } from '../../../../utils/client';
+import { client } from '../../../utils/client';
 export default async function handler(req= NextApiRequest, res= NextApiResponse) {
   if (req.method === 'GET') {
     const query = allPostsQuery();
