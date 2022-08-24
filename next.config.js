021e1d27fded1d4be006c7b665b34f1594cd263d
AbdullahLaham <82181597+AbdullahLaham@users.noreply.github.com>
2022-08-24 09:32:36 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: false,
   trailingSlash: true,
+  env: {
+    base_url: 'http://localhost:3000',
+  },
   swcMinify: true,
   images: {
     domains: ['yth3.ggpht.com', 'lh3.googleusercontent.com'],
